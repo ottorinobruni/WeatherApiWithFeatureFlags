9d2510aa692c5de54ecea18577a381f19068c8d0
Ottorino Bruni <ottorino.bruni@gmail.com>
2024-11-23 08:29:45 +0100

    removed connection

--- WeatherApiWithFeatureFlags/obj/Debug/net8.0/WeatherApiWithFeatureFlags.AssemblyInfo.cs
+++ WeatherApiWithFeatureFlags/obj/Debug/net8.0/WeatherApiWithFeatureFlags.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApiWithFeatureFlags")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b4bd856b510c82a451743758a66346c100dff6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0325ee5b1a69401be4c94b02defd92de19f25e06")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApiWithFeatureFlags")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApiWithFeatureFlags")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
