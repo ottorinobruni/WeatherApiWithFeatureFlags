0325ee5b1a69401be4c94b02defd92de19f25e06
Ottorino Bruni <ottorino.bruni@gmail.com>
2024-11-23 08:10:53 +0100

    add custom filter

--- WeatherApiWithFeatureFlags/Program.cs
+++ WeatherApiWithFeatureFlags/Program.cs
@@ -2,12 +2,21 @@ using Microsoft.FeatureManagement;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddFeatureManagement();
+builder.Configuration.AddAzureAppConfiguration(options => {
+    var connectionString = builder.Configuration.GetConnectionString("AppConfig");
+    options.Connect(connectionString).UseFeatureFlags();
+});
+
+builder.Services.AddAzureAppConfiguration();
+builder.Services.AddFeatureManagement().AddFeatureFilter<WeekdayFilter>();
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseAzureAppConfiguration();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,9 +32,15 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", async (IFeatureManager featureManager) =>
+app.MapGet("/weatherforecast", async (IFeatureManager featureManager, IConfiguration configuration) =>
 {
     var isDetailed = await featureManager.IsEnabledAsync("DetailedWeatherForecast");
+
+    // Get All Filters
+    var featureFilters = configuration.GetSection("FeatureManagement:DetailedWeatherForecast").GetChildren().ToList();
+
+    // Check for active filters
+    var areFiltersActive = featureFilters.Any();
 
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
@@ -33,7 +48,8 @@ app.MapGet("/weatherforecast", async (IFeatureManager featureManager) =>
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)],
-            isDetailed ? $"Detailed data for day {index}" : null
+            isDetailed ? $"Detailed data for day {index}" : null,
+            areFiltersActive ? "Filter Active" : null
         ))
         .ToArray();
     return forecast;
@@ -43,7 +59,18 @@ app.MapGet("/weatherforecast", async (IFeatureManager featureManager) =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary, string? Details)
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary, string? Details, string? AppliedFilters)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+[FilterAlias("Weekday")]
+public class WeekdayFilter : IFeatureFilter
+{
+    public Task<bool> EvaluateAsync(FeatureFilterEvaluationContext context)
+    {
+        var today = DateTime.Now.DayOfWeek;
+        bool isWeekday = today != DayOfWeek.Saturday && today != DayOfWeek.Sunday;
+        return Task.FromResult(isWeekday);
+    }
 }
